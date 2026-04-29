9b00d955d3e127027f9404f3f4bbb97024690474
fm331502 <fm331502@fi365.ort.edu.uy>
2026-04-29 12:49:40 -0300

    Update

--- server/v1/routes/user.routes.js
+++ server/v1/routes/user.routes.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { createUser, loginUser } from "../controllers/user.controller.js";
 import { validateBody } from "../middlewares/validateBody.middleware.js";
-import { createUserSchema, loginUserSchema } from "../validators/user.validator.js";
+import {
+  createUserSchema,
+  loginUserSchema,
+} from "../validators/user.validator.js";
 
 const router = Router({ mergeParams: true });
 
-router.post("/", validateBody(createUserSchema) createUser);
+router.post("/", validateBody(createUserSchema), createUser);
 router.post("/login", validateBody(loginUserSchema), loginUser);
 
 export default router;

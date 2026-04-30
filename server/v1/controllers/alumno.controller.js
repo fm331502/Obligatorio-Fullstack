5aec1c3bc0e6bcee07630238b1097dea36767d11
fm331502 <fm331502@fi365.ort.edu.uy>
2026-04-30 12:44:24 -0300

    agregar alumnos service y controller

--- server/v1/controllers/alumno.controller.js
+++ server/v1/controllers/alumno.controller.js
@@ -1,19 +1,27 @@
-export const getAlumnos = (req, res) => {
-  res.json({ message: "Alumno encontrados", alumnos: [] });
+import { GrupoService } from "../services/alumno.service.js";
+import { Res } from "../utils/response.utils.js";
+
+export const createAlumno = async (req, res) => {
+  const alumno = await GrupoService.create(req.body);
+  Res.ok(res, "Alumno creado", { alumno });
 };
 
-export const getAlumno = (req, res) => {
-  res.json({ message: "Alumno encontrado", alumno: {} });
+export const updateAlumno = async (req, res) => {
+  const alumno = await GrupoService.update(req.params.id, req.body);
+  Res.ok(res, "Alumno editado", { alumno });
 };
 
-export const createAlumno = (req, res) => {
-  res.json({ message: "Alumno creado!" });
+export const deleteAlumno = async (req, res) => {
+  await GrupoService.delete(req.params.id);
+  Res.ok(res, "Alumno eliminado");
 };
 
-export const updateAlumno = (req, res) => {
-  res.json({ message: "Alumno editado!" });
+export const getAlumno = async (req, res) => {
+  const alumno = await GrupoService.getById(req.params.id);
+  Res.ok(res, "Alumno encontrado", { alumno });
 };
 
-export const deleteAlumno = (req, res) => {
-  res.json({ message: "Alumno eliminado!" });
+export const getAlumnos = async (_, res) => {
+  const grupos = await GrupoService.getAll();
+  Res.ok(res, "Alumnos encontrados", { alumnos });
 };
---
+++ server/v1/services/alumno.service.js
@@ -0,0 +1,33 @@
+import Alumno from "../models/alumno.model.js";
+
+export class GrupoService {
+  static create = async (data) => {
+    const alumno = new Alumno(data);
+    await alumno.save();
+    return alumno;
+  };
+
+  static update = async (id, data) => {
+    validateObjectId(id);
+    const alumno = await Alumno.findByIdAndUpdate(id, data, {
+      returnDocument: "after",
+    });
+
+    if (!alumno) throw new NotFoundError("Alumno no encontrado");
+    return alumno;
+  };
+
+  static delete = async (id) => {
+    validateObjectId(id);
+    return await Alumno.findByIdAndDelete(id);
+  };
+
+  static getAll = async () => {
+    return await Alumno.find();
+  };
+
+  static getById = async (id) => {
+    validateObjectId(id);
+    return await Alumno.findById(id);
+  };
+}
